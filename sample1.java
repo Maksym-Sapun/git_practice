faa3680643f6b1a4f6e2370f5036fb3b09356244
TenBarrel6 <ruslan21140@gmail.com>
2024-04-19 19:48:33 -0600

    added my changes to the new feature

--- sample1.java
+++ sample1.java
@@ -1,6 +1,7 @@
-public class sample1{ 
-	public static void main (String[] args){
-		System.out.println("Hello World!");		
+public class sample1 {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
 		System.out.println("Your collaborator added the change");
+		System.out.println("Ruslan added another change");
 	}
 }
